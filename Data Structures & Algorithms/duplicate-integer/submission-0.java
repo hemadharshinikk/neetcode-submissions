1a203eb45fc18dfb948fc1e5b7a5ae73a0d7e0e6
Hemadharshini K <hemadharshini837@gmail.com>
2026-09-18 22:27:44 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer>set = new HashSet<>();
+        
+        for(int x:nums)
+        {
+            if(set.contains(x))
+            {
+                return true;
+            }
+            set.add(x);
+        }
+        return false;
+       
+    }
+}
